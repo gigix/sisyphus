0f6c656430b2ce51439cb601e052fa2a01ea358a
Jeff Xiong <gigix1980@gmail.com>
2013-02-10 20:45:53 +1100

    Create a frontend application

---
+++ frontend/assets/www/js/app.js
@@ -0,0 +1,9 @@
+angular.module('sisyphus', []).
+	config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+			when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl}).
+	      	when('/today', {templateUrl: 'partials/today.html',   controller: TodayCtrl}).
+	      	when('/rocks', {templateUrl: 'partials/rocks.html',   controller: RocksCtrl}).
+	      	when('/stats', {templateUrl: 'partials/stats.html',   controller: StatsCtrl}).
+	      	otherwise({redirectTo: '/'});
+	}]);
---
+++ frontend/assets/www/js/controllers.js
@@ -0,0 +1,15 @@
+function HomeCtrl() {
+	
+}
+
+function TodayCtrl($scope, $routeParams) {
+	// TODO: logic here
+}
+
+function RocksCtrl() {
+	
+}
+
+function StatsCtrl() {
+	
+}
