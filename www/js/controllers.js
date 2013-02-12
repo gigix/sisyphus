81f4406bfe72404b88608853e4ca1a100142b1b7
Jeff Xiong <gigix1980@gmail.com>
2013-02-12 20:35:16 +1100

    make it nicer

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,25 @@
+function NavCtrl($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };        
+}
+
 function HomeCtrl() {
 	
 }
 
-function TodayCtrl($scope, $routeParams) {
-	// TODO: logic here
+function TodayCtrl($scope) {
+	$scope.rocks = [{
+		'title': '解决测试跑两遍的问题',
+		'done': true
+	}, {
+		'title': 'Governance meeting deck',
+		'done': false
+	}, {
+		'title': 'Go through technical innovation initiatives',
+		'done': false
+	}];
 }
 
 function RocksCtrl() {
